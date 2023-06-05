d1be668407f1b0e0bb72976c9a0970c4f28a25c0
winNft <cookietiger89@icloud.com>
2023-06-05 17:39:30 +0200

    revert

--- components/faq/Faq.jsx
+++ components/faq/Faq.jsx
@@ -3,27 +3,35 @@ import Heading2 from "../headings/Heading2";
 import Heading5 from "../headings/Heading5";
 import SingleFAQ from "../singleFAQ/SingleFAQ";
 
-const Faq = () => (
-  <section className="mt-[70px] md:mt-[115px] px-4 2xl:px-0" id="faq">
-    <div className="max-w-[1296px] m-auto">
-      <div className="w-full lg:w-1/2 m-auto text-center">
-        <Heading5>Questions & Answers</Heading5>
-        <Heading2>Frequently Asked Questions </Heading2>
-        <p className="mt-[10px]">
-          Get all your questions answered at our Q&amp;A Corner. Whether you have burning inquiries or frequently asked questions, we&apos;ve got you covered. Find the information you need and gain clarity on any topic. Don&apos;t miss out on our comprehensive Q&amp;A resource. Explore now and get the answers you seek.
-        </p>
-      </div>
+const Faq = () => {
+  return (
+    <section className="mt-[70px] md:mt-[115px] px-4 2xl:px-0" id="faq">
+      <div className="max-w-[1296px] m-auto">
+        <div className="w-full lg:w-1/2 m-auto text-center">
+          <Heading5>Questions & Answers</Heading5>
+          <Heading2>Frequently Asked Questions </Heading2>
+          <p className="mt-[10px]">
+          Get all your questions answered at our Q&A Corner. Whether you have burning inquiries or frequently asked questions, we've got you covered. Find the information you need and gain clarity on any topic. Don't miss out on our comprehensive Q&A resource. Explore now and get the answers you seek.
+          </p>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 lg:gap-5 mt-[35px] lg:mt-[55px]">
-        <div className="flex flex-col gap-3 lg:gap-6">
-          {faqData.map((singleFaq, ind) => ind % 2 === 0 && <SingleFAQ {...singleFaq} key={singleFaq.id} />)}
-        </div>
-        <div className="flex flex-col gap-3 lg:gap-6">
-          {faqData.map((singleFaq, ind) => ind % 2 !== 0 && <SingleFAQ {...singleFaq} key={singleFaq.id} />)}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 lg:gap-5 mt-[35px] lg:mt-[55px]">
+          <div className="flex flex-col gap-3 lg:gap-6">
+            {faqData.map(
+              (singleFaq, ind) =>
+                ind % 2 === 0 && <SingleFAQ {...singleFaq} key={singleFaq.id} />
+            )}
+          </div>
+          <div className="flex flex-col gap-3 lg:gap-6">
+            {faqData.map(
+              (singleFaq, ind) =>
+                ind % 2 !== 0 && <SingleFAQ {...singleFaq} key={singleFaq.id} />
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Faq;
--- components/lottery/Lottery.jsx
+++ components/lottery/Lottery.jsx
@@ -4,22 +4,25 @@ import LotteryCard from "../cards/LotteryCard";
 import Heading2 from "../headings/Heading2";
 import Heading5 from "../headings/Heading5";
 
-const Lottery = () => (
-  <section className="mt-[70px] sm:mt-[115px] px-4 xl:px-0" id="lottery">
-    <div className="max-w-[1296px] m-auto flex flex-col lg:flex-row items-center gap-0 md:gap-5 lg:gap-0">
-      <LotteryCard />
-      <div className="w-1/12 hidden lg:block"></div>
-      <div className="w-full md:w-10/12 lg:w-6/12 mt-[35px] md:mt-[55px]">
-        <Heading5>Welcome to Mint and Win</Heading5>
-        <div className="mt-[10px]">
-          <Heading2>Mint and Win NFT Lottery Platform</Heading2>
-        </div>
-        <p className="mt-[15px]">
-          Welcome to Mint and Win, where valuable prizes await you! With 1000 exclusive NFTs, we&apos;re giving away 100 incredible prizes. Even if you don&apos;t win, stay tuned for exciting plans involving non-winning tickets. Join us and mint your NFTs promptly. Good luck on your rewarding journey with Mint and Win!
-        </p>
-        <p className="mt-[10px]">
+const Lottery = () => {
+  return (
+    <section className="mt-[70px] sm:mt-[115px] px-4 xl:px-0" id="lottery">
+      <div className="max-w-[1296px] m-auto flex flex-col lg:flex-row items-center gap-0 md:gap-5 lg:gap-0">
+        <LotteryCard />
+
+        <div className="w-1/12 hidden lg:block"></div>
+
+        <div className="w-full md:w-10/12 lg:w-6/12 mt-[35px] md:mt-[55px]">
+          <Heading5>Welcome to Mint and Win</Heading5>
+          <div className="mt-[10px]">
+            <Heading2>Mint and Win NFT Lottery Platform</Heading2>
+          </div>
+          <p className="mt-[15px]">
+Welcome to Mint and Win, where valuable prizes await you! With 1000 exclusive NFTs, we&apos;re giving away 100 incredible prizes. Even if you don&apos;t win, stay tuned for exciting plans involving non-winning tickets. Join us and mint your NFTs promptly. Good luck on your rewarding journey with Mint and Win!
+</p>
+
+          <p className="mt-[10px]">
           We eagerly anticipate your participation. Here is the list of prizes:
-<<<<<<< HEAD
           </p>
 
           <div className="flex gap-2 flex-wrap justify-center lg:justify-start rounded-[15px] bg-[#2A3246] p-[20px_15px] sm:p-[30px_20px] mt-[10px]">
@@ -28,7 +31,7 @@ const Lottery = () => (
               ["2nd", "10"],
               ["3rd", "3"],
               ["4th-28th", "1"],
-              ["29th-49th", "0.5"],
+              ["29th-50th", "0.5"],
               ["50th-100th", "0.32"],
             ].map(([place, bnb], i) => (
               <p key={i} className="border-r pr-2 last:border-0">
@@ -46,32 +49,10 @@ const Lottery = () => (
               JOIN OUR DISCORD
             </DiscordButton>
           </div>
-=======
-        </p>
-        <div className="flex gap-2 flex-wrap justify-center lg:justify-start rounded-[15px] bg-[#2A3246] p-[20px_15px] sm:p-[30px_20px] mt-[10px]">
-          {[
-            ["1st", "25"],
-            ["2nd", "10"],
-            ["3rd", "3"],
-            ["4th-28th", "1"],
-            ["29th-50th", "0.5"],
-            ["50th-100th", "0.32"],
-          ].map(([place, bnb], i) => (
-            <p key={i} className="border-r pr-2 last:border-0">
-              {place} place: <span className="text-[color:var(--color-primary)]">{bnb}BNB</span>
-            </p>
-          ))}
-        </div>
-        <div className="mt-[30px] md:mt-[40px]">
-          <DiscordButton>
-            <FaDiscord className="text-[24px]" />
-            JOIN OUR DISCORD
-          </DiscordButton>
->>>>>>> 15f8bfe13e7ab3c58f4e04cf11ee42f2313b6b41
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Lottery;
