b301224f6f22acc6cfb7a22800278ccc49736e13
winNft <cookietiger89@icloud.com>
2023-06-06 22:37:24 +0200

    upcoming fix background and position

--- components/upcoming/Upcoming.jsx
+++ components/upcoming/Upcoming.jsx
@@ -4,19 +4,19 @@ import Heading2 from "../headings/Heading2";
 
 const Upcoming = () => {
   return (
-    <section className="mt-[70px] sm:mt-[115px]">
-      <div className="bg-[color:var(--bg-gray)] text-center p-[60px_0_295px] md:p-[105px_0_345px]">
+    <div>
+      <div className="bg-[color:var(--bg-dark)] text-center p-[60px_0_295px] md:p-[105px_0_345px]">
         <Heading2>Upcoming Collections</Heading2>
       </div>
 
       <div className="max-w-[1296px] m-auto -mt-64 px-4 2xl:px-0">
-        <div className="flex justify-center flex-wrap gap-5">
+        <div className="flex justify-between flex-wrap gap-2">
           {upcomingData.map((item) => (
             <UpcomingLotteryCard key={item.id} {...item} />
           ))}
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
