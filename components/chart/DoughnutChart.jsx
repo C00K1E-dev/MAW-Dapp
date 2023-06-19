714d03355f82e4f0e5a2361ab52df20826ed9f4c
winNft <cookietiger89@icloud.com>
2023-06-19 20:26:01 +0200

    color

--- components/chart/DoughnutChart.jsx
+++ components/chart/DoughnutChart.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import 'chart.js/auto';
-import Heading2 from "../headings/Heading2";
-import Heading5 from "../headings/Heading5";
 
 const data = {
   labels: ['Presale 10% 15 million tokens', 'Staking Rewards 20% 30 million tokens', 'Team 10% 15 million tokens', 'Marketing (including airdrops) 20% 30 million tokens', 'Liquidity Pools 20% 30 million tokens', 'Development Fund 10% 15 million tokens', 'Reserve 10% 15 million tokens'],
@@ -11,13 +9,13 @@ const data = {
       label: 'Tokenomics Distribution',
       data: [15 , 30, 15, 30, 30, 10, 10],
       backgroundColor: [
-        '#000000',     
-        '#01161d',     
-        '#022b3b',     
-        '#034353',     
-        '#045d6b',     
-        '#057484',     
-        '#0adab9',     
+        '#33e2c9',     
+        '#008f95',     
+        '#d1d5e3',     
+        '#9fa4bd',     
+        '#546093',     
+        '#2c2f45',     
+        '#5b6173',     
       ],
       borderColor: [
         '#0adab9',     
