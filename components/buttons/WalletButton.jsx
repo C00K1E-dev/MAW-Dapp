799602cd80a51b2f2d0f669ce923df7016cd94a6
winNft <cookietiger89@icloud.com>
2023-06-22 21:00:05 +0200

    wallet

--- components/buttons/WalletButton.jsx
+++ components/buttons/WalletButton.jsx
@@ -12,44 +12,51 @@ const WalletButton = ({ children, clr, hrf = "/", contractAddress }) => {
     checkConnectionStatus();
   }, []);
 
-  async function checkConnectionStatus() {
+  function checkConnectionStatus() {
     if (window.ethereum) {
-      try {
-        // Get provider and signer
-        const provider = new Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
+      handleEthereum();
+    } else {
+      window.addEventListener("ethereum#initialized", handleEthereum, {
+        once: true,
+      });
 
-        // Check if there is a connected account
-        const accounts = await provider.listAccounts();
+      // If the event is not dispatched by the end of the timeout,
+      // the user probably doesn't have MetaMask installed.
+      setTimeout(handleEthereum, 3000); // 3 seconds
+    }
+  }
+
+  function handleEthereum() {
+    const { ethereum } = window;
+    if (ethereum && ethereum.isMetaMask) {
+      console.log("Ethereum successfully detected!");
+      // Get provider and signer
+      const provider = new Web3Provider(ethereum);
+      const signer = provider.getSigner();
+
+      // Check if there is a connected account
+      provider.listAccounts().then((accounts) => {
         const isConnected = accounts.length > 0;
 
         // Set state
         setProvider(provider);
         setAddress(isConnected ? accounts[0] : null);
         setConnected(isConnected);
+      });
 
-        // Add event listener for disconnect and account changes
-        window.ethereum.on("disconnect", handleDisconnect);
-        window.ethereum.on("accountsChanged", function (accounts) {
-          if (accounts.length === 0) {
-            setConnected(false);
-            setAddress(null);
-          } else {
-            setConnected(true);
-            setAddress(accounts[0]);
-          }
-        });
-      } catch (error) {
-        console.error(error);
-      }
+      // Add event listener for account changes
+      ethereum.on("accountsChanged", function (accounts) {
+        if (accounts.length === 0) {
+          setConnected(false);
+          setAddress(null);
+        } else {
+          setConnected(true);
+          setAddress(accounts[0]);
+        }
+      });
     } else {
-      console.error("Metamask not detected");
+      console.log("Please install MetaMask!");
     }
-  }
-
-  function handleDisconnect(error) {
-    setConnected(false);
-    setAddress(null);
   }
 
   async function connectToMetamask() {
@@ -69,6 +76,17 @@ const WalletButton = ({ children, clr, hrf = "/", contractAddress }) => {
         setProvider(provider);
         setAddress(address);
         setConnected(true);
+
+        // Add event listener for account changes
+        window.ethereum.on("accountsChanged", function (accounts) {
+          if (accounts.length === 0) {
+            setConnected(false);
+            setAddress(null);
+          } else {
+            setConnected(true);
+            setAddress(accounts[0]);
+          }
+        });
       } catch (error) {
         console.error(error);
       }
