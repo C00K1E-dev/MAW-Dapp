77bd21791e1a3b51211551bae90234ea727fd1b3
winNft <cookietiger89@icloud.com>
2023-08-01 22:06:02 +0200

    _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { arbitrum, avalanche, bscTestnet, mainnet, polygon } from 'wagmi/chains';
 import { Web3Modal } from '@web3modal/react';
+import { useEffect, useState } from "react";
 
 const chains = [arbitrum, mainnet, polygon, bscTestnet, avalanche];
 const projectId = 'aca932c97e3f9bc59a1636dc1aeae670';
@@ -15,28 +16,22 @@ const wagmiConfig = createConfig({
 });
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+
   return (
-    
-      <WagmiConfig config={wagmiConfig}>
-        <Web3Modal
-          themeMode="dark"
-          themeVariables={{
-            '--w3m-logo-image-url': './images/walletlogo.png',
-            '--w3m-overlay-backdrop-filter': 'blur(5px)',
-            '--w3m-overlay-background-color': 'rgba(0, 0, 0, 0.1)',
-            '--w3m-background-color': '#0adab9',
-            '--w3m-accent-fill-color': '#1a1f2c',
-            '--w3m-accent-color': '#0adab9',
-            '--w3m-font-family': 'jost',
-          }}
-          projectId={projectId}
-          ethereumClient={ethereumClient}
-        />
-        <Component {...pageProps} />
-      </WagmiConfig>
-    
+    <>
+      {ready ? (
+        <WagmiConfig config={wagmiConfig}>
+          <Component {...pageProps} />
+        </WagmiConfig>
+      ) : null}
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
   );
 }
-
-export default MyApp;
