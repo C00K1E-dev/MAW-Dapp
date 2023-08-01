565001d6dcee7ebc539845ab6e9320386d645031
winNft <cookietiger89@icloud.com>
2023-08-01 09:23:08 +0200

    appwrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { arbitrum, avalanche, bscTestnet, mainnet, polygon } from 'wagmi/chains';
+
+const chains = [arbitrum, mainnet, polygon, bscTestnet, avalanche];
+const projectId = 'aca932c97e3f9bc59a1636dc1aeae670';
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    
+      <WagmiConfig config={wagmiConfig}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+    
+  );
 }
 
-export default MyApp
+export default MyApp;
