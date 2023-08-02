fa88e8acb8d418b0db787ad49b88a794845cd04d
winNft <cookietiger89@icloud.com>
2023-08-02 11:38:10 +0200

    mintattempt

--- components/buttons/MintVipExclusive.jsx
+++ components/buttons/MintVipExclusive.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect } from "react";
 import { useAccount } from "wagmi";
 import Web3 from "web3";
 import PopupMessage from "../PopupMessage";
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { arbitrum, avalanche, bscTestnet, mainnet, polygon } from 'wagmi/chains';
+
+const chains = [arbitrum, mainnet, polygon, bscTestnet, avalanche];
+const projectId = 'aca932c97e3f9bc59a1636dc1aeae670';
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 const contractAddress = "0x27e25A7630a9C68c97aF93c394EE788E6c0160F8";
 const abi = [
@@ -563,6 +576,12 @@ const abi = [
   ]
 
   export default function MintVipExclusive() {
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+      setReady(true);
+    }, []);
+    
     const { isConnected } = useAccount();
     const [showPopup, setShowPopup] = useState(false);
     const [popupMessage, setPopupMessage] = useState("");
@@ -610,9 +629,11 @@ const abi = [
   
     return (
         <div>
-          <button className="btn btn--primary" onClick={handleMint}>
+          <WagmiConfig config={wagmiConfig}>
+          <button className="btn btn--primary" ethereumClient={ethereumClient} onClick={handleMint}>
             Mint Test
           </button>
+          </WagmiConfig>
     
           {showPopup && (
             <PopupMessage message={popupMessage} onClose={() => setShowPopup(false)} />
