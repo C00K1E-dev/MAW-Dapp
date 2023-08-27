67b1f0c4634f0d0c65011bc29337986625b8d93a
C00K1E-dev <andrei.g@mintandwin.com>
2023-08-27 13:53:29 +0200

    whitepaper

--- components/token/token.jsx
+++ components/token/token.jsx
@@ -8,16 +8,24 @@ const Token = () => {
   return (
     <section className="mt-[70px] sm:mt-[115px] px-4 xl:px-0" id="token">
       <div className="max-w-[1296px] m-auto">
-        <div className="text-center mb-20"> {/* Add mb-8 for margin-bottom */}
+        <div className="text-center mb-20">
           <Heading2>Token</Heading2>
         </div>
 
         <div className="max-w-[1296px] m-auto flex flex-col lg:flex-row items-center gap-0 md:gap-5 lg:gap-0">
 
           <div className="w-full md:w-10/12 lg:w-6/12 mt-[-40px] md:mt-[-100px]">
-            <div className="flex flex-col items-center"> {/* Center the title */}
+            <div className="flex flex-col items-center">
               <Heading5>Mint and Win Token</Heading5>
               <Headingcontract>0x4546dFFA8dC8D1D7044BEC469650a95306C2C086</Headingcontract>
+              
+              {/* Button to open the Whitepaper */}
+              <a href="/docs/Whitepaper.pdf" target="_blank" rel="noopener noreferrer">
+                <button className="btn btn--primary">
+                  View Whitepaper
+                </button>
+              </a>
+
               <div className="mt-[-100px] md:mt-[20px]" style={{ width: '100%', height: '500px' }}>
                 <ApacheChart />
               </div>
