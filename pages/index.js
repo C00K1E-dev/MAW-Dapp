f2c5181ae4f632648c368f27bfb247f48c37558a
C00K1E-dev <andrei.g@mintandwin.com>
2024-03-10 18:32:25 +0100

    removed modal working

--- pages/index.js
+++ pages/index.js
@@ -17,35 +17,34 @@ import VIP from "../components/VIP/VIP";
 import Token from "../components/token/token";
 import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 import { WagmiConfig } from "wagmi";
-import { arbitrum,avalanche,bsc,fantom,mainnet,optimism,polygon,} from "wagmi/chains";
+import { arbitrum, avalanche, bsc, fantom, mainnet, optimism, polygon } from "wagmi/chains";
 import Modal from '../components/modal/Modal';
 
-
-const chains = [bsc,mainnet,polygon,avalanche,arbitrum,optimism,fantom,];
+const chains = [bsc, mainnet, polygon, avalanche, arbitrum, optimism, fantom];
 const projectId = "aca932c97e3f9bc59a1636dc1aeae670";
 const metadata = {
-	name: "Mint and Win",
-	description: "Mint and Win",
-	url: "https://mintandwin.com",
-	icons: './images/walletlogo.png',
+  name: "Mint and Win",
+  description: "Mint and Win",
+  url: "https://mintandwin.com",
+  icons: './images/walletlogo.png',
 };
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
 
-createWeb3Modal({metadata, wagmiConfig, projectId, chains, themeMode:'dark', themeVariables: {
+createWeb3Modal({ metadata, wagmiConfig, projectId, chains, themeMode: 'dark', themeVariables: {
   '--w3m-accent-fill-color': '#1a1f2c',
   '--w3m-accent': '#0adab9',
   '--w3m-font-family': 'jost',
 }});
 
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false); // Set initial state to false
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
   useEffect(() => {
-    setIsModalOpen(true);
+    setIsModalOpen(false); // Set to false on mount
   }, []);
 
   return (
@@ -59,7 +58,6 @@ const Home = () => {
       </Head>
       <WagmiConfig config={wagmiConfig}> {/* Wrap the entire content with WagmiConfig */}
         <main>
-          <createWeb3Modal/>
           <NavBar />
           <CookieConsent />
           <Hero />
@@ -78,7 +76,7 @@ const Home = () => {
           <Modal isOpen={isModalOpen} closeModal={closeModal} />
         </main>
       </WagmiConfig>
-      </div>
+    </div>
   );
 };
 
