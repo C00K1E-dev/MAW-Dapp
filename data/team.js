a010d93661a5dfae7cac5331b7f64a6f8af52778
C00K1E-dev <andrei.g@mintandwin.com>
2024-04-02 21:45:51 +0200

    removed team member

--- data/team.js
+++ data/team.js
@@ -1,29 +1,14 @@
 import team1 from "../public/images/team/team1.png";
-import team2 from "../public/images/team/team2.png";
-import team33 from "../public/images/team/team33.png";
-
 
 export const teamData = [
   {
     id: "1",
     img: team1,
     name: "Andrei Galea",
-    designation: "Product Owner",
+    designation: "Product Owner and Developer",
     linkedin: "https://www.linkedin.com/in/andrei-galea-a2a8b9278/",
   },
-  {
-    id: "2",
-    img: team2,
-    name: "Darius Galea",
-    designation: "CMO",
-    linkedin: "https://www.linkedin.com/in/darius-galea-a3072418b/",
-  },
-  {
-    id: "3",
-    img: team33,
-    name: "Mariana Lup",
-    designation: "Digital Marketer",
-    
-  },
+
+
 
 ];
